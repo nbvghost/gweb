579c59d0e6cc5f34fbe86948a263b02fe66f5f56
nbvghost <nbvghost@qq.com>
2019-11-28 10:07:35 +0800

    link list 排序

--- tool/collections/link_list.go
+++ tool/collections/link_list.go
@@ -8,6 +8,7 @@ import (
 type KV struct {
 	Key   string
 	Value string
+	Previous  *KV
 	Next  *KV
 }
 
@@ -19,107 +20,141 @@ type LinkList struct {
 	Last     *KV
 	Map      map[string]*KV
 }
+func (v *LinkList) SortAscFunc(a *KV,b *KV) int{
+	if a ==nil || b==nil{
+		return 0
+	}
+	if b.Next==nil{
+		return 0
+	}
+	if a.Key < b.Key{
 
+		return 1
+
+	}else if a.Key > b.Key{
+
+		return -1
+
+	}else{
+
+		if a.Value < b.Value{
+			return 1
+
+		}else if a.Value > b.Value{
+			return -1
+		}else{
+			//==0
+			return 0
+
+		}
+
+	}
+}
+func (v *LinkList) SortDescFunc(a *KV,b *KV) int{
+	if a ==nil || b==nil{
+		return 0
+	}
+	if b.Next==nil{
+		return 0
+	}
+	if a.Key < b.Key{
+
+		return -1
+
+	}else if a.Key > b.Key{
+
+		return 1
+
+	}else{
+
+		if a.Value < b.Value{
+			return -1
+
+		}else if a.Value > b.Value{
+			return 1
+		}else{
+			//==0
+			return 0
+
+		}
+
+	}
+}
 //todo:还没有写完
-func (v *LinkList) SortDesc() {
-
+func (v *LinkList) Sort(sortDescFunc func(a *KV,b *KV)int){
 	//desc -1
 	//asc 1
 	//eque 0
-	sortFunc:= func(a *KV,b *KV) int{
-		if a ==nil || b==nil{
-			return 0
-		}
-		if b.Next==nil{
-			return 0
-		}
+	v.Last=nil
+	nextNode:=v.RootNode
+
+	for nextNode!=nil{
+		var swpNode *KV
+		if nextNode.Next!=nil{
+			sortType:=sortDescFunc(nextNode,nextNode.Next)
+			if sortType ==-1{
+				isRootNode:=false
+				if nextNode.Previous==nil{
+					isRootNode=true
+				}
+
+				previousNode:=nextNode.Previous
+
+				upNode:=nextNode.Next
+				upNodeChild:=nextNode.Next.Next
+
+				upNode.Previous=previousNode//
+				nextNode.Previous=upNode
+				upNode.Next=nextNode
+
+				upNodeChild.Previous=upNode.Next
+				upNode.Next.Next=upNodeChild
 
 
+				if isRootNode{
+					upNode.Previous=nil
+					v.RootNode=upNode
+				}else{
+					previousNode.Next=upNode
+				}
+				swpNode =upNode
 
-		if a.Key < b.Next.Key{
-
-			return -1
-
-		}else if a.Key > b.Next.Key{
-
-			return 1
-
-		}else{
-
-			if a.Value < b.Next.Value{
-				return -1
-
-			}else if a.Key > b.Next.Key{
-				return 1
-			}else{
-				//==0
-				return 0
-
-			}
-
-		}
-
-	}
-	if sortFunc(nil,nil)>0{
-
-	}
-
-	readNode := v.RootNode
-	for readNode != nil{
-
-		if readNode.Next==nil{
-			break
-		}
-
-
-		//sortType:=sortFunc()
-
-
-		if readNode.Key < readNode.Next.Key{
-
-			Key   :=readNode.Key
-			Value :=readNode.Value
-
-			readNode.Key=readNode.Next.Key
-			readNode.Value=readNode.Next.Value
-
-			readNode.Next.Key=Key
-			readNode.Next.Value=Value
-
-
-
-
-
-
-		}else if readNode.Key > readNode.Next.Key{
-
-
-
-		}else{
-			//==0
-			if readNode.Value < readNode.Next.Value{
-
-				Key   :=readNode.Key
-				Value :=readNode.Value
-
-				readNode.Key=readNode.Next.Key
-				readNode.Value=readNode.Next.Value
-
-				readNode.Next.Key=Key
-				readNode.Next.Value=Value
-
-			}else if readNode.Key > readNode.Next.Key{
-
-			}else{
-				//==0
+			}else if sortType ==1{
 
 			}
 
+			if swpNode==nil{
+				nextNode=nextNode.Next
+			}else{
+				if swpNode.Previous==nil{
+					nextNode=v.RootNode
+				}else{
+					nextNode=swpNode.Previous
+				}
+			}
+		}else{
+
+			newLastNode:=nextNode.Previous
+			if nextNode==v.Last{
+				break
+			}
+			v.Last = newLastNode
+
+			lastNode:=nextNode
+			lastNode.Previous.Next=nil
+			lastNode.Previous=nil
+
+			v.RootNode.Previous=lastNode
+
+			lastNode.Next=v.RootNode
+			v.RootNode=lastNode
+
+			nextNode=v.RootNode
+
 		}
 
-		readNode = readNode.Next
-
 	}
+
 }
 func (v *LinkList) Get(key string) *KV {
 	if v.Map == nil {
@@ -133,25 +168,23 @@ func (v *LinkList) Get(key string) *KV {
 	return &KV{}
 }
 
-func (v *LinkList) Add(key, value string) {
+func (v *LinkList) Add(key, value string) *KV {
 	if v.Map == nil {
 		v.Map = make(map[string]*KV)
 	}
-	item, ok := v.Map[key]
-	if ok {
-		item.Value = value
-	} else {
+	//item, ok := v.Map[key]
+
 		node := &KV{Value: value, Key: key}
 		v.Map[key] = node
 		if v.RootNode == nil {
 			v.RootNode = node
 			v.Last = node
 		}else if v.Last != nil {
+			node.Previous=v.Last
 			v.Last.Next = node
 			v.Last = node
 		}
-
-	}
+		return node
 }
 func (v *LinkList) GetMap() map[string]string {
 	data := make(map[string]string)
